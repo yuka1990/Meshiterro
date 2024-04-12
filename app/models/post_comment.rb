d483c3f99f1051a50c163e738c2205962848260d
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-12 03:26:28 +0000

    [update]18章まで

---
+++ app/models/post_comment.rb
@@ -0,0 +1,9 @@
+class PostComment < ApplicationRecord
+
+
+
+belongs_to :user
+belongs_to :post_image
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  resources :post_images, only: [:new, :create,:index, :show,:destroy]
+  resources :post_images, only: [:new, :create,:index, :show,:destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only:[:show, :edit,:update]
 
   get 'homes/about' => 'homes#about', as: "about"
